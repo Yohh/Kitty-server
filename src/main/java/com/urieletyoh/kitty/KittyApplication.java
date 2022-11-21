7ce6bd877ee2ab5af91ed6ec0a3df448f358e6bb
Yohh <durandyohan@zaclys.net>
2022-11-21 09:44:10 +0100

    first commit

---
+++ src/main/java/com/urieletyoh/kitty/KittyApplication.java
@@ -0,0 +1,13 @@
+package com.urieletyoh.kitty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KittyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urieletyoh/kitty/KittyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urieletyoh.kitty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KittyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
