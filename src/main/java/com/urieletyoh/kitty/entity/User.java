3c9425f86632d6ccaaad91876077b20c67d8ea77
Yohh <durandyohan@zaclys.net>
2022-11-21 10:12:21 +0100

    add User.java, add properties in application.properties, add .env and .env.sample

---
+++ src/main/java/com/urieletyoh/kitty/entity/User.java
@@ -0,0 +1,19 @@
+package com.urieletyoh.kitty.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    public User(){}
+
+}
