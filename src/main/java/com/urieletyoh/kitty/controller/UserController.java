23a003cf5ff838c8ceaf5b7bbc05bbf73b9654d8
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-11-23 11:09:35 +0100

    login ok

--- src/main/java/com/urieletyoh/kitty/controller/UserController.java
+++ src/main/java/com/urieletyoh/kitty/controller/UserController.java
@@ -3,6 +3,11 @@ package com.urieletyoh.kitty.controller;
 import com.urieletyoh.kitty.entity.User;
 import com.urieletyoh.kitty.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +17,7 @@ import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
@@ -19,13 +25,16 @@ public class UserController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    @GetMapping("/api/users")
+    @Autowired
+    public AuthenticationManager manager;
+
+    @GetMapping("/users")
     public List<User> getUsers() {
         List<User> users  = repository.findAll();
         return users;
     }
 
-    @GetMapping("/api/users/{userId}")
+    @GetMapping("/users/{userId}")
     public User getUser(@PathVariable Long userId) {
         User user = new User();
         if(userId != null) {
@@ -37,7 +46,7 @@ public class UserController {
         return user;
     }
 
-    @PostMapping("/api/register")
+    @PostMapping("/register")
     public User addUser(@RequestBody User user) {
         String clearTextPassword = user.getPassword();
         user.setPassword(passwordEncoder.encode(clearTextPassword));
@@ -45,13 +54,17 @@ public class UserController {
         return newUser;
     }
 
-    @RequestMapping("/api/login")
-    public boolean login(@RequestBody User user) {
-            Optional<User> optionalUser = Optional.ofNullable(repository.findByUsername(user.getUsername()));
+    @RequestMapping("/login")
+    public String login(@RequestBody User user) {
+           /* Optional<User> optionalUser = Optional.ofNullable(repository.findByUsername(user.getUsername()));
             if(optionalUser.isPresent()) {
                 BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
                 return passwordEncoder.matches(user.getPassword(), optionalUser.get().getPassword());
             }
-            return false;
+            return false; */
+        System.out.println(manager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())));
+       Authentication authentication = manager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+       return authentication.getName();
     }
 }
